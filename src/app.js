ea9272894fdc04395fb0dde6488601f2bdc75db5
Vidhi Mody <vidhimody98@gmail.com>
2019-12-22 22:36:49 +0530

    Set up for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -66,7 +68,7 @@ app.get('*',(req, res) => {
     res.render('404')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up and running')
 })
 
